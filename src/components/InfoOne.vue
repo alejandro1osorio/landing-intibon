260e57afec8e227844d1833b3ae16cb4c6df8687
alejandro1osorio <alejowosorio2986@gmail.com>
2025-02-26 07:55:37 -0500

    movil info1

--- src/components/InfoOne.vue
+++ src/components/InfoOne.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="parent" id="50color">
     <div class="div1">
-      <div class="top-line"></div>
-      <h1 class="title">Protector Solar con Color <strong>SPF50</strong></h1>
-      <p class="description">Protección avanzada contra los rayos UV con un toque de color para un acabado natural.</p>
+      <h1 class="title">Higiene <strong style="border-bottom: 5px solid #008EB4;">Íntima</strong></h1>
+      <p class="description">Los temas de cuidado íntimo deben ser parte de la charla con sus pacientes, <strong>para crear <br> consciencia sobre la higiene íntimay ayudar a mejorar su salud y bienestar en general.<sup>4</sup></strong></p>
     </div>
     <div class="div2">
       <img src="../assets/otros/tema1.png" alt="Placeholder Image" class="image" />
@@ -47,16 +46,6 @@ export default {
   margin: 0;
   position: relative;
   text-align: center;
-}
-
-.div1 .top-line {
-  position: absolute;
-  top: 35px;
-  left: 50%;
-  width: 20%;
-  height: 5px;
-  background-color: orange;
-  transform: translateX(-50%);
 }
 
 .div1 .title {
@@ -138,7 +127,7 @@ export default {
   .parent {
     grid-template-columns: repeat(6, 1fr);
     grid-template-rows: auto;
-    height: 110vh;
+    height: 140vh;
   }
 
   .div1 {
@@ -155,6 +144,11 @@ export default {
 
   .div4 {
     grid-area: 3 / 1 / 4 / 7;
+  }
+
+  .div4 .image-centered {
+    max-width: 100%;
+    max-height: 100%;
   }
 
   .div3 {
