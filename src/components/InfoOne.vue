b009474546fb069054c46aeb8f47965125695aa3
alejandro1osorio <alejowosorio2986@gmail.com>
2025-02-26 07:48:14 -0500

    test mobile

--- src/components/InfoOne.vue
+++ src/components/InfoOne.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="parent">
-    <div class="div1">Header</div>
-    <div class="div2">Subheader</div>
-    <div class="div3">
+  <div class="parent" id="50color">
+    <div class="div1">
+      <div class="top-line"></div>
+      <h1 class="title">Protector Solar con Color <strong>SPF50</strong></h1>
+      <p class="description">Protección avanzada contra los rayos UV con un toque de color para un acabado natural.</p>
+    </div>
+    <div class="div2">
       <img src="../assets/otros/tema1.png" alt="Placeholder Image" class="image" />
     </div>
     <div class="div4">
-      <img src="../assets/productos/grupo-productos1.png" alt="Sidebar Image" class="image" />
+      <img src="../assets/logos-sellos/grupo-sellos.png" alt="Placeholder Image" class="image-centered" />
     </div>
-    <div class="div5">
-      <img src="../assets/logos-sellos/grupo-sellos.png" alt="Extra Content Image" class="image" />
+    <div class="div3">
+      <img src="../assets/productos/grupo-productos1.png" alt="Placeholder Image" class="image-centered" />
     </div>
   </div>
 </template>
@@ -23,76 +26,139 @@ export default {
 <style scoped>
 .parent {
   display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  grid-template-rows: repeat(7, 1fr);
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
   width: 100%;
   height: 100vh;
-  padding: 10px;
+  padding: 0;
   box-sizing: border-box;
 }
 
 .div1 {
-  grid-area: 1 / 1 / 2 / 8;
-  background-color: #ff5733;
+  grid-area: 1 / 1 / 2 / 13;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin: 0;
+  position: relative;
   text-align: center;
-  padding: 20px;
-  font-weight: bold;
+}
+
+.div1 .top-line {
+  position: absolute;
+  top: 35px;
+  left: 50%;
+  width: 20%;
+  height: 5px;
+  background-color: orange;
+  transform: translateX(-50%);
+}
+
+.div1 .title {
+  font-size: 2em;
+  font-weight: 300;
+  color: #214F79;
+}
+
+.div1 .description {
+  font-size: 1em;
+  color: #214F79;
+  margin-top: 10px;
+  text-align: center;
+  max-width: 80%;
 }
 
 .div2 {
-  grid-area: 2 / 2 / 3 / 7;
-  background-color: #33c3ff;
-  text-align: center;
-  padding: 20px;
-}
-
-.div3 {
-  grid-area: 3 / 1 / 8 / 6;
-  background-color: #75ff33;
-  text-align: center;
-  padding: 20px;
+  grid-area: 2 / 1 / 6 / 8;
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 100%;
+  margin: 0;
 }
 
-.div3 .image {
+.div2 .image {
   width: 100%;
   height: 100%;
   object-fit: contain;
 }
 
 .div4 {
-  grid-area: 4 / 6 / 8 / 8;
-  background-color: #ff33a1;
-  text-align: center;
-  padding: 20px;
+  grid-area: 2 / 9 / 3 / 12;
+  color: white;
   display: flex;
   align-items: center;
   justify-content: center;
+  font-size: 1.5em;
+  height: 80px;
+  margin: 0;
 }
 
-.div4 .image {
-  width: 100%;
-  height: 100%;
+.div4 .image-centered {
+  max-width: 140%;
+  max-height: 140%;
   object-fit: contain;
 }
 
-.div5 {
-  grid-area: 3 / 6 / 4 / 8;
-  background-color: #ffdb33;
-  text-align: center;
-  padding: 20px;
+.div3 {
+  grid-area: 3 / 9 / 5 / 12;
+  color: white;
   display: flex;
   align-items: center;
   justify-content: center;
+  font-size: 1.5em;
+  height: 100%;
+  margin: 0;
 }
 
-.div5 .image {
-  width: 100%;
-  height: 100%;
+.div3 .image-centered {
+  max-width: 100%;
+  max-height: 100%;
   object-fit: contain;
+}
+
+@media screen and (min-width: 1400px) {
+  .div4 {
+    height: 110px;
+  }
+}
+
+@media screen and (min-width: 1024px) and (max-width: 1399px) {
+  .div4 {
+    height: 90px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .parent {
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: auto;
+    height: 110vh;
+  }
+
+  .div1 {
+    grid-area: 1 / 1 / 2 / 7;
+  }
+
+  .div1 .title {
+    font-size: 1.5em;
+  }
+
+  .div2 {
+    grid-area: 2 / 1 / 3 / 7;
+  }
+
+  .div4 {
+    grid-area: 3 / 1 / 4 / 7;
+  }
+
+  .div3 {
+    grid-area: 4 / 1 / 5 / 7;
+  }
 }
 </style>
